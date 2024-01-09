d3d74b319a1a6defdd8f1c9df6df3dcad74bf7b3
h-kinocoboy <h.kinocoboy@gmail.com>
2024-01-09 23:33:56 +0900

    feat: hello-worldgit add -A!

---
+++ my-app/resources/views/hello.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>My First Page</title>
+</head>
+<body>
+  <h2>Hello World!!</h2>
+  <p>おらおら</p>
+<p>Dec/16/2023</p>
+</body>
+</html>
